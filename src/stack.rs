7fb37e630a8b7a1d8cade2b3bcd712ab6bc78ce4
Kalaimani Balu <kalaimani.balu@gmail.com>
2021-05-09 13:11:08 +0530

    Code Reformat

--- src/stack.rs
+++ src/stack.rs
@@ -1,4 +1,3 @@
-
 pub struct List<T> {
     head: Link<T>,
     len: usize,
@@ -17,7 +16,7 @@ impl<T> List<T> {
     }
 
     pub fn push(&mut self, elem: T) {
-        self.head = Some(Box::new(Node{ elem, next: self.head.take() }));
+        self.head = Some(Box::new(Node { elem, next: self.head.take() }));
         self.len += 1;
     }
 
@@ -58,7 +57,7 @@ impl<T> Iterator for IntoIter<T> {
 }
 
 pub struct Iter<'a, T> {
-    next: Option<&'a Node<T>>
+    next: Option<&'a Node<T>>,
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
@@ -73,7 +72,7 @@ impl<'a, T> Iterator for Iter<'a, T> {
 }
 
 pub struct IterMut<'a, T> {
-    next: Option<&'a mut Node<T>>
+    next: Option<&'a mut Node<T>>,
 }
 
 impl<'a, T> Iterator for IterMut<'a, T> {
@@ -93,11 +92,11 @@ impl<T> List<T> {
     }
 
     pub fn iter(&self) -> Iter<T> {
-        Iter{ next: self.head.as_deref() }
+        Iter { next: self.head.as_deref() }
     }
 
     pub fn iter_mut(&mut self) -> IterMut<T> {
-        IterMut{ next: self.head.as_deref_mut() }
+        IterMut { next: self.head.as_deref_mut() }
     }
 }
 
@@ -133,7 +132,9 @@ mod test {
         let mut list = List::new();
         assert_eq!(list.peek(), None);
         assert_eq!(list.peek_mut(), None);
-        list.push(1); list.push(2); list.push(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
@@ -149,7 +150,9 @@ mod test {
     #[test]
     fn into_iter() {
         let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         let mut iter = list.into_iter();
         assert_eq!(iter.next(), Some(3));
@@ -161,7 +164,9 @@ mod test {
     #[test]
     fn iter() {
         let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         let mut iter = list.iter();
         assert_eq!(iter.next(), Some(&3));
@@ -172,7 +177,9 @@ mod test {
     #[test]
     fn iter_mut() {
         let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
         let mut iter = list.iter_mut();
         assert_eq!(iter.next(), Some(&mut 3));
